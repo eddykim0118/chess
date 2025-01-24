b5beb5873b97f113ba2fdff3a1286f3c9e95ca6c
Eddy Kim <eddykim0118@gmail.com>
2025-01-24 16:58:04 -0700

    wip

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -17,8 +17,40 @@ public class ChessBoard {
         
     }
 
+
+    
+    /**
+     * Adds a chess piece to the chessboard
+     *
+     * @param position where to add the piece to
+     * @param piece    the piece to add
+     */
+    public void addPiece(ChessPosition position, ChessPiece piece) {
+        if (isValidPosition(position)) {
+            board[position.getRow() - 1][position.getColumn() - 1] = piece;
+        }
+    }
+
+    /**
+     * Gets a chess piece on the chessboard
+     *
+     * @param position The position to get the piece from
+     * @return Either the piece at the position, or null if no piece is at that
+     * position
+     */
+    public ChessPiece getPiece(ChessPosition position) {
+        if (isValidPosition(position)) {
+            return board[position.getRow() - 1][position.getColumn() - 1];
+        }
+        return null;
+    }
+
+    /**
+     * Sets the board to the default starting board
+     * (How the game of chess normally starts)
+     */
     public void resetBoard() {
-        // Clear the board first
+            // Clear the board first
         board = new ChessPiece[8][8];
 
         // Setup White Pieces (Bottom of board - row 1)
@@ -51,39 +83,7 @@ public class ChessBoard {
             addPiece(new ChessPosition(7, i), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
         }
     }
-    /**
-     * Adds a chess piece to the chessboard
-     *
-     * @param position where to add the piece to
-     * @param piece    the piece to add
-     */
-    public void addPiece(ChessPosition position, ChessPiece piece) {
-        if (isValidPosition(position)) {
-            board[position.getRow() - 1][position.getColumn() - 1] = piece;
-        }
-    }
-
-    /**
-     * Gets a chess piece on the chessboard
-     *
-     * @param position The position to get the piece from
-     * @return Either the piece at the position, or null if no piece is at that
-     * position
-     */
-    public ChessPiece getPiece(ChessPosition position) {
-        if (isValidPosition(position)) {
-            return board[position.getRow() - 1][position.getColumn() - 1];
-        }
-        return null;
-    }
-
-    /**
-     * Sets the board to the default starting board
-     * (How the game of chess normally starts)
-     */
-    public void resetBoard() {
-        throw new RuntimeException("Not implemented");
-    }
+    
 
     /**
      * Checks if a position is within the valid bounds of the chess board
