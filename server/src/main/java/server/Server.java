7937ba634b87bb39db714fd6126c564c575e7934
Eddy Kim <eddykim0118@gmail.com>
2025-03-03 21:48:08 -0700

    changes and endpoints made

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,31 @@
 package server;
 
+import com.google.gson.Gson;
+import dataAccess.*;
+import model.*;
+import service.*;
+import service.requests.*;
 import spark.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Server {
+    private final UserService userService;
+    private final GameService gameService;
+    private final ClearService clearService;
+    private final Gson gson = new Gson();
+
+    public Server() {
+        UserDAO userDAO = new MemoryUserDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        
+        // Initialize Services
+        userService = new UserService(userDAO, authDAO);
+        gameService = new GameService(gameDAO, authDAO);
+        clearService = new ClearService(userDAO, gameDAO, authDAO);
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,13 +33,27 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", this::clearApplication);
+        Spark.post("/user", this::register);
+        Spark.post("/session", this::login);
+        Spark.delete("/session", this::logout);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
+        Spark.exception(DataAccessException.class, this::handleException);
         Spark.init();
-
         Spark.awaitInitialization();
         return Spark.port();
     }
+    
+
+
+
+
+
+
 
     public void stop() {
         Spark.stop();
