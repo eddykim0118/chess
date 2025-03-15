fc32883f5c93bafa0252925282d413396cabc7e0
Eddy Kim <eddykim0118@gmail.com>
2025-03-14 21:02:38 -0600

    wip

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -19,14 +19,15 @@ public class MySQLUserDAO implements UserDAO {
     @Override
     public void createUser(UserData user) throws DataAccessException {
         try (var conn = DatabaseManager.getConnection();
-             var stmt = conn.prepareStatement("INSERT INTO users (username, password, email) VALUES (?, ?, ?)")) {
+            var stmt = conn.prepareStatement("INSERT INTO users (username, password, email) VALUES (?, ?, ?)")) {
                 stmt.setString(1, user.username());
                 String hashPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
                 stmt.setString(2, hashPassword);
                 stmt.setString(3, user.email());
-             } catch (SQLException e) {
+                stmt.executeUpdate(); // Execute the statement
+            } catch (SQLException e) {
                 throw new DataAccessException("Error creating user" + e.getMessage());
-             }
+            }
     }
 
     @Override
