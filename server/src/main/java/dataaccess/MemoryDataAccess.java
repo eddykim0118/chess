633f997d438eb871736adf202540968f27ca20c8
Weon Geun Kim <weongeunkim@Weons-MacBook-Pro.local>
2025-07-15 23:53:12 -0600

    MemoryDataAccess started - I'm not sure if this is the right way yet

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -1,10 +1,24 @@
 package dataaccess;
 
 import model.UserData;
-import model.GameData
+import model.GameData;
+import model.AuthData;
 
 import java.util.Map;
+import java.util.HashMap;
 
-public class MemoryDataAccess {
-    private final Map<String, UserData> users =
+public class MemoryDataAccess implements DataAcess {
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<String, AuthData> authTokens = new HashMap<>();
+    private final Map<String, GameData> games = new HashMap<>();
+
+    private int nextGameID = 1;
+
+    @Override
+    public void clear() throws DataAccessException {
+        users.clear();
+        authTokens.clear();
+        games.clear();
+        nextGameID = 1;
+    }
 }
