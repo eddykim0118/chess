aeb75f606c81d5c6c768c85111d919ed72d9e193
Weon Geun Kim <weongeunkim@Weons-MacBook-Pro.local>
2025-08-12 14:46:09 -0600

    placing the test file in the right directory

--- server/src/test/java/passoff/server/TestFactory.java
+++ server/src/test/java/passoff/server/TestFactory.java
@@ -1,4 +1,4 @@
-package passoff.chess.server;
+package passoff.server;
 
 import com.google.gson.GsonBuilder;
 
--- server/src/test/java/passoff/server/WebSocketTests.java
+++ server/src/test/java/passoff/server/WebSocketTests.java
@@ -1,4 +1,4 @@
-package passoff.chess.server;
+package passoff.server;
 
 import chess.*;
 import org.junit.jupiter.api.*;
